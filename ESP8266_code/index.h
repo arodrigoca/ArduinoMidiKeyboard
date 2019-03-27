8c268c5bcf73d8e25ca70d3f3ab9f338964da1ed
arodrigoca <adri_rodri_64@hotmail.es>
2019-03-27 21:31:37 +0100

    update

--- ESP8266_code/index.h
+++ ESP8266_code/index.h
@@ -2,21 +2,39 @@ const char MAIN_page[] PROGMEM = R"=====(
 	<!DOCTYPE html>
 	<html>
 	<head>
-    <title>Sunvox Controller</title>
+		<title>Sunvox Controller</title>
 		<style>
 			#main_header{
-				color: blue;
+				-webkit-text-fill-color: #6ca3fc;
+				-webkit-text-stroke: 1px black;
+				font-family: Arial;
 			}
 
-			button{
+			button.but_class_1{
 				background-color: #4CAF50; /* Green */
-	  		border: none;
-	  		color: white;
-	  		padding: 15px 32px;
-	  		text-align: center;
-	  		text-decoration: none;
-	  		display: inline-block;
-	  		font-size: 24px;
+				border: none;
+				color: white;
+				padding: 15px 32px;
+				text-align: center;
+				text-decoration: none;
+				display: inline-block;
+				font-size: 24px;
+				border-radius: 12px;
+				border: 2px solid blue; /* Green */
+				box-shadow: 0 9px #999;
+				width: 100%;
+				padding: 50px;
+			}
+
+			button.but_class_2{
+				background-color: #e8a0d2; /* Green */
+				border: none;
+				color: white;
+				padding: 15px 32px;
+				text-align: center;
+				text-decoration: none;
+				display: inline-block;
+				font-size: 24px;
 				border-radius: 12px;
 				border: 2px solid blue; /* Green */
 				box-shadow: 0 9px #999;
@@ -26,7 +44,12 @@ const char MAIN_page[] PROGMEM = R"=====(
 
 			button:active{
 				box-shadow: 0 5px #666;
-	  		transform: translateY(4px);
+				transform: translateY(4px);
+			}
+
+			#last_command{
+				-webkit-text-fill-color: #3371d6;
+				font-family: Arial;
 			}
 		</style>
 	</head>
@@ -34,29 +57,39 @@ const char MAIN_page[] PROGMEM = R"=====(
 
 	<div>
 	<h1 id="main_header">Sunvox Controller</h1>
-		<div>
-			<button type="button" onclick="sendData('gen_up')">Generator Up</button>
-		</div>
-		<br>
-		<div>
-			<button type="button" onclick="sendData('gen_down')">Generator Down</button><BR>
-		</div>
-	</div>
+	<table>
+		<tr>
+			<th>
+				<button class="but_class_1" type="button" onclick="sendData('gen_up')">Generator Up</button>
+			</th>
+			<th>
+				<button class="but_class_2" type="button" onclick="sendData('gen_down')">Generator Down</button><BR>
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<button class="but_class_1" type="button" onclick="sendData('vol_up')">Volume Up</button>
+			</th>
+			<th>
+				<button class="but_class_2" type="button" onclick="sendData('vol_down')">Volume Down</button>
+			</th>
+		</tr>
+	</table>
 	<br>
 	<div>
-	    <span id="last_command">NA</span>
+			<span id="last_command">NA</span>
 	</div>
 	<script>
 	function sendData(command) {
-	  var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	      document.getElementById("last_command").innerHTML =
-	      this.responseText;
-	    }
-	  };
-	  xhttp.open("GET", "send_command?command="+command, true);
-	  xhttp.send();
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("last_command").innerHTML =
+				this.responseText;
+			}
+		};
+		xhttp.open("GET", "send_command?command="+command, true);
+		xhttp.send();
 	}
 
 	</script>
