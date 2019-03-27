7fbc14ab47c793c43e13e7fa49e1316ba5c691c5
arodrigoca <adri_rodri_64@hotmail.es>
2019-03-27 18:55:14 +0100

    update

--- RPI_Code/Deprecated_code/serial_to_midi.py
+++ RPI_Code/Deprecated_code/serial_to_midi.py
@@ -1,18 +1,54 @@
 import serial
 import mido
+import time
 
-s = serial.Serial('/dev/ttyUSB0', 115200, timeout = None)
+
+def init_serial():
+
+    global s
+    s = serial.Serial('/dev/ttyUSB0', 115200, timeout = None)
+
+
+def check_connection():
+
+    try:
+
+        init_serial()
+
+    except serial.serialutil.SerialException:
+
+        print('MIDI device not connected. Retrying in 1 second')
+        time.sleep(1)
+        #check_connection()
+
+
+def main():
+
+    print('MIDI device is connected. Ready for input!')
+    try:
+
+        while True:
+
+            message = s.read(3)
+            msg_output = mido.Message.from_bytes(message)
+            #print(msg_output)
+            outport.send(msg_output)
+
+    except KeyboardInterrupt:
+
+        s.close()
+        print('Closing program!')
+
+    except serial.serialutil.SerialException:
+
+        print('MIDI device has been disconnected. Attemping to reconnect...')
+        s.close()
+        check_connection()
+        main()
+
+
+check_connection()
 #print(mido.get_output_names())
-mido.open_output()
+#mido.open_output()
 outport = mido.open_output()
-try:
-    while True:
-
-        message = s.read(3)
-        msg_output = mido.Message.from_bytes(message)
-        #print(msg_output)
-        outport.send(msg_output)
-
-except KeyboardInterrupt:
-
-    s.close()
+main()
