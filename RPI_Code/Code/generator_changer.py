cb8f5a7526052e2202c2106cea8ffcf1f8b47e45
arodrigoca <adri_rodri_64@hotmail.es>
2019-03-30 14:46:36 +0100

    update. This time correctly XD

--- RPI_Code/Code/generator_changer.py
+++ RPI_Code/Code/generator_changer.py
@@ -1,55 +1,122 @@
-#!/usr/bin/env python3
+import time
+import keyboard
+from bluetooth import *
+import subprocess
 
-"""
-	File: pyBluezServer.py
-	Install packages:
-		apt-get install bluez libbluetooth-dev
-	Install python module:
-		pip3 install pybluez
-	Show bluetooth devices:
-		hciconfig -a
+global volume
+volume = 70
 
-	Configure the bluetooth interface:
-		hciconfig hci0 piscan
-"""
+def init_keyboard():
 
-import bluetooth
-import os
-import sys
+    keyboard.press("shift")
+    keyboard.release("shift")
 
 
-def print_help():
-	script_name = os.path.basename(__file__)
-	print('USAGE: ' + script_name + ' <server_port>')
+def config_bt():
+
+    global server_sock
+    server_sock=BluetoothSocket( RFCOMM )
+    server_sock.bind(("",PORT_ANY))
+    server_sock.listen(1)
+
+    global port
+    port = server_sock.getsockname()[1]
+
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+    advertise_service( server_sock, "SampleServer",
+                       service_id = uuid,
+                       service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                       profiles = [ SERIAL_PORT_PROFILE ])
 
 
-def main(argv):
+def generator_up():
 
-	if len(argv) < 2:
-		print_help()
-		exit(1)
-
-	server_bt_port = int(argv[1])
-
-	server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
-	server_bt_address = '', server_bt_port
-
-	server_sock.bind(server_bt_address)
-	server_sock.listen(1)
-
-	while True:
-
-		client_sock,address = server_sock.accept()
-		print('Accepted connection from ', address)
-
-		data = client_sock.recv(1024)
-		print('Received: ' + data.decode('utf-8'))
-
-		client_sock.close()
-
-	server_sock.close()
+    print("Gen Up")
+    keyboard.send("ctrl+UP")
 
 
-if __name__ == '__main__':
-main(sys.argv)
+def generator_down():
+
+    print("Gen Down")
+    keyboard.send("ctrl+DOWN")
+
+
+def volume_change(command):
+
+    print("Volume up command")
+    volume_cmd = "amixer sset 'Speaker' " + command + "%"
+    volume = command
+    print(volume)
+    subprocess.call(volume_cmd, shell=True)
+
+
+def mute_command(mute):
+
+    print("Mute button")
+
+    if(mute.mute == False):
+        subprocess.call("amixer sset 'Speaker' mute", shell=True)
+        mute.mute = True
+
+    else:
+        subprocess.call("amixer sset 'Speaker' unmute", shell=True)
+        mute.mute = False
+        print("Unmuted")
+
+
+
+def start_bt():
+
+    print("Waiting for connection on RFCOMM channel %d" % port)
+    global client_sock, client_info
+    client_sock, client_info = server_sock.accept()
+
+    print("Accepted connection from ", client_info)
+    class mute_object:
+
+        def __init__(self):
+            self.mute = False
+
+    mute = mute_object()
+
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        #print("received [%s]" % data)
+        #print(data.decode("utf-8"))
+        command = data.decode("utf-8")
+        if(command == "gen_up"):
+            generator_up()
+
+        elif(command == "gen_down"):
+            generator_down()
+
+
+        elif(command == "mute"):
+
+            mute_command(mute)
+
+        else:
+
+            volume_change(command)
+
+
+
+subprocess.call("amixer sset 'Speaker' 70%", shell=True)
+init_keyboard()
+config_bt()
+
+try:
+    start_bt()
+
+except IOError:
+
+    client_sock.close()
+    start_bt()
+
+
+except KeyboardInterrupt:
+
+    client_sock.close()
+    server_sock.close()
