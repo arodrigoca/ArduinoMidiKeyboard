9fb15cd18b051cb30011393e87928936f6145105
arodrigoca <adri_rodri_64@hotmail.es>
2019-03-30 17:19:08 +0100

    update. now reconnect and connecting bt works

--- RPI_Code/Code/generator_changer.py
+++ RPI_Code/Code/generator_changer.py
@@ -12,24 +12,6 @@ def init_keyboard():
     keyboard.release("shift")
 
 
-def config_bt():
-
-    global server_sock
-    server_sock=BluetoothSocket( RFCOMM )
-    server_sock.bind(("",PORT_ANY))
-    server_sock.listen(1)
-
-    global port
-    port = server_sock.getsockname()[1]
-
-    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-
-    advertise_service( server_sock, "SampleServer",
-                       service_id = uuid,
-                       service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                       profiles = [ SERIAL_PORT_PROFILE ])
-
-
 def generator_up():
 
     print("Gen Up")
@@ -66,55 +48,67 @@ def mute_command(mute):
 
 
 
-def start_bt():
-
-    print("Waiting for connection on RFCOMM channel %d" % port)
-    global client_sock, client_info
-    client_sock, client_info = server_sock.accept()
-
-    print("Accepted connection from ", client_info)
-    class mute_object:
-
-        def __init__(self):
-            self.mute = False
-
-    mute = mute_object()
-
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        #print("received [%s]" % data)
-        #print(data.decode("utf-8"))
-        command = data.decode("utf-8")
-        if(command == "gen_up"):
-            generator_up()
-
-        elif(command == "gen_down"):
-            generator_down()
-
-
-        elif(command == "mute"):
-
-            mute_command(mute)
-
-        else:
-
-            volume_change(command)
-
-
-
 subprocess.call("amixer sset 'Speaker' 70%", shell=True)
 init_keyboard()
-config_bt()
 
 try:
-    start_bt()
 
-except IOError:
+    server_sock=BluetoothSocket( RFCOMM )
+    server_sock.bind(("",PORT_ANY))
+    server_sock.listen(1)
 
-    client_sock.close()
-    start_bt()
+    port = server_sock.getsockname()[1]
 
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+    advertise_service( server_sock, "TestServer",
+                       service_id = uuid,
+                       service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                       profiles = [ SERIAL_PORT_PROFILE ],
+    #                   protocols = [ OBEX_UUID ]
+                        )
+
+    print ("Waiting for connection on RFCOMM channel %d" % port)
+
+    client_sock, client_info = server_sock.accept()
+    print ("Accepted connection from ", client_info)
+
+    while True:
+
+
+        try:
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            command = data.decode("utf-8")
+            if(command == "gen_up"):
+                generator_up()
+
+            elif(command == "gen_down"):
+                generator_down()
+
+
+            elif(command == "mute"):
+
+                mute_command(mute)
+
+            else:
+
+                volume_change(command)
+
+        except IOError:
+
+            client_sock, client_info = server_sock.accept()
+            print ("Accepted connection from ", client_info)
+
+        except KeyboardInterrupt:
+
+            print ("disconnected")
+
+            client_sock.close()
+            server_sock.close()
+            print ("all done")
+
+            break
 
 except KeyboardInterrupt:
 
